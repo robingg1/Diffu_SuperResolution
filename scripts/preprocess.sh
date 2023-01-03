63653a9925efb02e0f3a3f228d96628fa5bd55c4
robingg1 <you@example.com>
2023-01-03 12:58:30 +0800

    init

---
+++ scripts/preprocess.sh
@@ -0,0 +1,9 @@
+input_dir=$1
+output=output
+filelist=`ls $input_dir` 
+for file in $filelist
+do
+  echo $input_dir$file
+  #python -m apps.prt_util -i $input_dir$file
+  python -m apps.render_data -i $input_dir$file -o $output -e
+done
